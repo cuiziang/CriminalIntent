0e726bae6eed827d698a0427f16f5d2a984961fa
Ziang Cui <mrchoisss@gmail.com>
2018-11-06 15:44:07 -0500

    Create chapter 9

---
+++ app/src/main/java/me/cuiziang/criminalintent/CrimeListActivity.java
@@ -0,0 +1,11 @@
+package me.cuiziang.criminalintent;
+
+
+import android.support.v4.app.Fragment;
+
+public class CrimeListActivity extends SingleFragmentActivity {
+    @Override
+    protected Fragment createFragment() {
+        return new CrimeListFragment();
+    }
+}
